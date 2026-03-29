4d55e53a7767aa4a9e81da1ce8d001a470d50f80
Fabioknzw <fabiopsilveira90@gmail.com>
2026-03-29 19:01:39 -0300

    segundo desfio DIO

---
+++ desafio2.js
@@ -0,0 +1,61 @@
+
+//criando a função para descobrir o saldo de vitoria do jogador
+function saldo_de_Rankeadas(){
+        let saldo= vitoria-derrota
+    return saldo
+
+}
+
+
+// declarando as variaveis
+
+let vitoria= 10
+let derrota= 0
+let saldoVitoria= saldo_de_Rankeadas()
+let nivel= ""
+
+// criando o laço de retição
+for(let i=0;i<1; i++){
+// criando o estrutura de repetição 
+    
+
+
+if (saldoVitoria<=10){
+    nivel="Ferro" 
+}
+else if(saldoVitoria>11 && saldoVitoria<=20){
+    nivel= "Bronze"
+}
+else if(saldoVitoria>20 && saldoVitoria<=50){
+    nivel= "Prata"
+}
+else if(saldoVitoria>50 && saldoVitoria<=80){
+    nivel= "Ouro"
+}
+else if(saldoVitoria>80 && saldoVitoria<=90){
+    nivel= "Diamante"
+}
+else if(saldoVitoria>90 && saldoVitoria<=100){
+    nivel= "Lendário"
+}
+else{nivel="Imortal"}
+
+// ecxibindo a saida
+console.log("  O Herói tem de saldo de "+saldoVitoria+" esta no nivel "+ nivel)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
